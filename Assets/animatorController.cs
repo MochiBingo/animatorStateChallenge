f019f926052e20e1a3d7167688104a719f3e3613
MochiBingo <mochibingoshep@gmail.com>
2025-02-27 15:25:31 -0400

    update

--- Assets/animatorController.cs
+++ Assets/animatorController.cs
@@ -5,37 +5,58 @@ using UnityEngine;
 
 public class animatorController : MonoBehaviour
 {
-    public Animator animator;
-    public Vector2 scrollValue = Vector2.zero;
+    public Animator boolAnimator;
+    public Animator scrollAnimator;
+    public Animator triggerAnimator;
+    public Material floatMat;
+    private Vector2 scrollValue = Vector2.zero;
+    public bool boolValue;
+    public float floatValue;
     void Start()
     {
-        
+        boolValue = false;
     }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            animator.SetTrigger("");
+            triggerAnimator.SetTrigger("TriggerVal");
+            Debug.Log("Triggered");
         }
         if (Input.GetKeyDown(KeyCode.Tab))
         {
-
+            if (boolValue == false)
+            {
+                boolValue = true;
+            }
+            if (boolValue == true)
+            {
+                boolValue = false;
+            }
         }
         if (Input.mouseScrollDelta.y < 1.0f)
         {
-
+            floatMat.color = Color.yellow;
         }
         else if (Input.mouseScrollDelta.y < 2.0f)
         {
-
+            floatMat.color = Color.green;
         }
         else if (Input.mouseScrollDelta.y < 3.0f)
         {
-
+            floatMat.color = Color.blue;
         }
         else if (Input.mouseScrollDelta.y > 3.0f)
         {
             //Input.mouseScrollDelta.y = 3.0f;
+        }
+        if (boolValue == false)
+        {
+            boolAnimator.SetBool("ActiveAnim", true);
+        }
+        if (boolValue == true)
+        {
+            boolAnimator.SetBool("ActiveAnim", false);
         }
     }
 }
